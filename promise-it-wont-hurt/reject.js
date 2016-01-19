c221aa681995ce86256df9e210188052e0d519db
d-smith <doug.smith.mail@gmail.com>
2016-01-19 11:11:54 -0500

    promises and scopes

---
+++ promise-it-wont-hurt/reject.js
@@ -0,0 +1,6 @@
+var q = require('q');
+var defer = q.defer();
+defer.promise.then(null, function(err){
+  console.log(err.message);
+});
+setTimeout(defer.reject, 300, new Error('REJECTED!'));
---
+++ scopes-chains-closures/scopes3.js
@@ -0,0 +1,7 @@
+function foo() {
+  var bar;
+  quux = 17;
+  function zip() {
+    var quux;
+  }
+}
