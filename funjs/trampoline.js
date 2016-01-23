c1f94383302ceee972b33d4b550f19cc2a7c3c23
d-smith <doug.smith.mail@gmail.com>
2016-01-23 08:31:51 -0700

    added trampoline

---
+++ funjs/trampoline.js
@@ -0,0 +1,20 @@
+function repeat(operation, num) {
+  return function() {
+    if (num <= 0) return
+    operation()
+    return repeat(operation, --num)
+  }
+}
+
+function trampoline(fn) {
+   while(fn && typeof fn === 'function') {
+     fn = fn()
+   }
+}
+
+module.exports = function(operation, num) {
+  // You probably want to call your trampoline here!
+  return trampoline(function() {
+    return repeat(operation, num)
+  })
+}
