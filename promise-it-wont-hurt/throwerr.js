c683933c6cb3394055a4c3a90ed99ddddeb3f398
d-smith <doug.smith.mail@gmail.com>
2016-01-30 07:08:45 -0800

    added throwerr.js

---
+++ promise-it-wont-hurt/throwerr.js
@@ -0,0 +1,20 @@
+var q = require('q');
+
+
+function parsePromised(s) {
+  var def = q.defer()
+    , result;
+
+  try {
+    result = JSON.parse(s);
+  } catch(e) {
+    def.reject(e);
+  }
+
+  def.resolve(result);
+  return def.promise;
+}
+
+
+parsePromised(process.argv[2])
+  .then(null, console.log);
