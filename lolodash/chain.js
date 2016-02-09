ced35913d8874aaf345a9f3eb223c41d7ecc829c
d-smith <doug.smith@fmr.com>
2016-02-09 10:01:45 -0800

    Added chain.js

---
+++ lolodash/chain.js
@@ -0,0 +1,15 @@
+var _ = require("lodash");
+
+var worker = function(words) {
+    return _.chain(words)
+            .map(function(word) {
+              return word + "chained";
+            })
+            .map(function(word){
+              return word.toUpperCase();
+            })
+            .sortBy()
+            .value();
+};
+
+module.exports = worker;
