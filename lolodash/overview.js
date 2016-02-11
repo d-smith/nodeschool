ab46b53e0f8ca86c17b24b1e7915e824d4c3ea05
d-smith <doug.smith@fmr.com>
2016-02-11 09:17:35 +0000

    Added overview

---
+++ lolodash/overview.js
@@ -0,0 +1,31 @@
+var _ = require("lodash");
+
+var worker = function(orders) {
+  var overview = [];
+  var total = 0;
+
+  orders = _.groupBy(orders, 'article');
+
+
+  _.forEach(orders, function(item, key){
+
+    key = parseInt(key);
+    total = 0;
+
+    if(item.length === 1) {
+      total = item[0].quantity;
+    } else {
+      total = _.reduce(item, function(sum, item){
+        return sum + item.quantity;
+      },0);
+    }
+    overview.push({article:key,total_orders:total})
+  });
+
+  overview = _.sortBy(overview,"total_orders").reverse();
+
+  return overview;
+
+};
+
+module.exports = worker;
